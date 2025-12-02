302e4eca03852b7baeb19a08985e0df4f3210546
Sai Prakash k <prakash387@yahoo.com>
2025-12-02 15:43:22 +0530

    changes

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -86,7 +86,7 @@ const Login = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
-                Login in
+                Login
               </button>
             </div>
           </form>
